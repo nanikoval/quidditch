620111eaa31306e480c435d2fc4f86b439ce922e
nanikoval <nataliakoval@live.com.ar>
2019-08-12 21:30:26 -0300

    Agregamos diagrama de clases y TestBonus

---
+++ src/test/java/TestBonus.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class TestBonus {
+
+    private Jugador jugador;
+    private Equipo equipo;
+    private Nimbus nimbus;
+    private List<Jugador> jugadores;
+    private Buscador buscador;
+    private Cazador cazador;
+    private SaetaDeFuego saetaDeFuego;
+    private Guardian guardian;
+
+    @BeforeEach
+    void setUp() {
+
+        equipo=mock(Equipo.class);
+        jugadores = new ArrayList<>();
+        nimbus=mock(Nimbus.class);
+        saetaDeFuego=new SaetaDeFuego("Saeta");
+        buscador = new Buscador(equipo, 10.0, 30.0, nimbus, 40.0,35.0);
+
+        jugadores.add(buscador);
+        equipo.setJugadores(jugadores);
+    }
+    @Test
+    void esGroso(){
+        when(buscador.habilidadJugador()).thenReturn(0.3);
+        when(equipo.promedioEquipo()).thenReturn(0.6);
+        when(nimbus.velocidadEscoba()).thenReturn(10.0);
+        when(nimbus.getVelocidadDefault()).thenReturn(0);
+        assertTrue(buscador.esGroso());
+    }
+
+    @Test
+    void buscadorEsGolpeadoPorUnaBludger(){
+        buscador.juega();
+        buscador.esGolpeadoPorUnaBludger();
+        assertEquals(buscador.getTurnosContinuos(),0);
+        assertEquals(buscador.getKilometrosRecorridos(), 0);
+    }
+
+
+
+}
